042507942a24ad41bf3648603beca1b454edcf5b
Pinank Solanki <pinanks@lambdatest.com>
2023-03-01 18:30:53 +0530

    Check for updates and deprecation warning

---
+++ commands/utils/package.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+var { constants } = require('./constants');
+
+// Check for package updates
+function checkUpdate(version, options) {
+    return axios.get(new URL(constants[options.env].CHECK_UPDATE_PATH, constants[options.env].BASE_URL).href, {
+        params: {
+            packageName: 'smartui-storybook',
+            packageVersion: version
+        }})
+        .then(function (response) {
+            if (response.data.data.deprecated) {
+                console.log('v' + version + ' is deprecated. Please update to v' + response.data.data.latestVersion);
+            } else if (response.data.data.latestVersion != version) {
+                console.log('A newer version v' + response.data.data.latestVersion + ' is available.');
+            }
+        })
+        .catch(function (error) {
+            if (error.response) {
+                console.log('Cannot check for updates. Error: ' + error.response.data.error.message);
+            } else {
+                console.log('Cannot check for updates. Error: ' + error.message);   
+            }
+        });
+};
+
+module.exports = { checkUpdate };
--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const program = new Command();
 const { storybook } = require('./commands/storybook');
 const { validateProjectToken, validateLatestBuild } = require('./commands/utils/validate');
 const { createConfig } = require('./commands/config');
+const { version } = require('./package.json');
+const { checkUpdate } = require('./commands/utils/package');
 
 program
     .name('smartui')
     .description('CLI to help you run your SmartUI tests on LambdaTest platform')
-    .version('1.1.2')
+    .version('v' + version)
     .addOption(new Option('--env <prod|stage>', 'Runtime environment option').choices(['prod', 'stage']));
 
 const configCommand = program.command('config')
@@ -18,7 +20,12 @@ const configCommand = program.command('config')
 configCommand.command('create')
     .description('Create LambdaTest SmartUI config file')
     .argument('[filepath]', 'Optional config filepath')
-    .action(function(filepath) {
+    .action(async function(filepath, options) {
+        options.env = program.opts().env || 'prod';
+        console.log('SmartUI Storybook CLI v' + version);
+        await checkUpdate(version, options);
+        console.log('\n');
+
         createConfig(filepath);
     });
 
@@ -29,6 +36,10 @@ program.command('storybook')
     // .option('--force-rebuild', 'Force a rebuild of an already existing build.', false)
     .action(async function(serve, options) {
         options.env = program.opts().env || 'prod';
+        console.log('SmartUI Storybook CLI v' + version);
+        await checkUpdate(version, options);
+        console.log('\n');
+        
         await validateProjectToken(options);
         // if (!options.forceRebuild) await validateLatestBuild(options);
         storybook(serve, options);
