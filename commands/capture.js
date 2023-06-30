7eba28c0135d71a9eaf2897998016387a021d8d2
sushobhit-lt <sushobhitd@lambdatest.com>
2023-06-30 12:34:44 +0530

    remove extra logs

--- commands/capture.js
+++ commands/capture.js
@@ -3,8 +3,6 @@ const { fetchDOM, upload } = require('./utils/dom')
 
 async function capture(screenshots, options) {
     await fetchDOM(screenshots,options);
-    console.log("done")
-    console.log('[smartui] Build in progress...');
     await upload(screenshots, options);
 }
 
