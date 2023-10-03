d8c7da667735b223a0c8d4f14a617697ed233e2b
Sushobhit Dua <38429226+sushobhit-lt@users.noreply.github.com>
2023-10-03 13:25:27 +0530

    Revert "fix validation errors and added logger"

--- commands/config.js
+++ commands/config.js
@@ -3,19 +3,19 @@ const fs = require('fs');
 const { defaultSmartUIConfig, defaultScreenshotConfig } = require('./utils/config');
 var { constants } = require('./utils/constants');
 
-function createConfig(filepath, logger) {
+function createConfig(filepath) {
     // default filepath
     filepath = filepath || '.smartui.json';
     let filetype = path.extname(filepath);
     if (filetype != '.json') {
-        logger.error(`[smartui] Error: Config file must have .json extension`);
+        console.log(`[smartui] Error: Config file must have .json extension`);
         process.exitCode = constants.ERROR_CATCHALL;
         return
     }
 
     // verify the file does not already exist
     if (fs.existsSync(filepath)) {
-        logger.error(`[smartui] Error: LambdaTest SmartUI config already exists: ${filepath}`);
+        console.log(`[smartui] Error: LambdaTest SmartUI config already exists: ${filepath}`);
         process.exitCode = constants.ERROR_CATCHALL;
         return
     }
@@ -23,22 +23,22 @@ function createConfig(filepath, logger) {
     // write stringified default config options to the filepath
     fs.mkdirSync(path.dirname(filepath), { recursive: true });
     fs.writeFileSync(filepath, JSON.stringify(defaultSmartUIConfig, null, 2) + '\n');
-    logger.info(`[smartui] Created LambdaTest SmartUI config: ${filepath}`);
+    console.log(`[smartui] Created LambdaTest SmartUI config: ${filepath}`);
 };
 
-function createScreenshotConfig(filepath, logger) {
+function createScreenshotConfig(filepath) {
     // default filepath
     filepath = filepath || 'screenshot.json';
     let filetype = path.extname(filepath);
     if (filetype != '.json') {
-        logger.error(`[smartui] Error: Config file must have .json extension`);
+        console.log(`[smartui] Error: Config file must have .json extension`);
         process.exitCode = constants.ERROR_CATCHALL;
         return
     }
 
     // verify the file does not already exist
     if (fs.existsSync(filepath)) {
-        logger.error(`[smartui] Error: Screenshot config already exists: ${filepath}`);
+        console.log(`[smartui] Error: Screenshot config already exists: ${filepath}`);
         process.exitCode = constants.ERROR_CATCHALL;
         return
     }
@@ -46,7 +46,7 @@ function createScreenshotConfig(filepath, logger) {
     // write stringified default config options to the filepath
     fs.mkdirSync(path.dirname(filepath), { recursive: true });
     fs.writeFileSync(filepath, JSON.stringify(defaultScreenshotConfig, null, 2) + '\n');
-    logger.info(`[smartui] Created Screenshot config: ${filepath}`);
+    console.log(`[smartui] Created Screenshot config: ${filepath}`);
 };
 
 module.exports = { createConfig, createScreenshotConfig };
