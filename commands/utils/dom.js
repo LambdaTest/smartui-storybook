31a7dfe1462ad25e9aeed33cd5c992ec55c1a5c7
shrinishLT <shrinishv@lambdatest.com>
2026-09-07 20:22:34 +0530

    Revert "TE-13434: print tag warnings returned by the render service"
    This reverts commit 96eb050b5eab56b1a68f9a339cc97994810f314e.

--- commands/utils/dom.js
+++ commands/utils/dom.js
@@ -85,7 +85,6 @@ async function sendDoM(storybookUrl, stories, storybookConfig, options) {
         })
         .then(async function (response) {
             console.log('[smartui] Build URL: ', response.data.buildURL);
-            (response.data.warnings || []).forEach(w => console.log('[smartui] Warning: ', w));
             console.log('[smartui] Build in progress...');
             await shortPolling(response.data.buildId, 0, options);
         })
